c9714d6b7cf2c29fa8d4611903b277b085e185ad
nullx <mail@nullx.me>
2020-01-18 01:33:15 +0100

    Initial Commit

---
+++ nullxscript/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nullxscript
+//
+//  Created by Jon Lara Trigo on 18/01/2020.
+//  Copyright © 2020 Jon Lara Trigo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
